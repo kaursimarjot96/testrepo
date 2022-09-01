e5bdea8aa3ec634b79e38a1245d1b880b77b0476
kaursimarjot96 <112201460+kaursimarjot96@users.noreply.github.com>
2022-09-01 15:26:26 -0400

    Add files via upload

---
+++ practice notebook.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "My Jupyter Notebook on IBM Watson Studio"
+
+# **Simarjot Kaur**
+# 
+# **I want to become a data scientist.**
+
+# *I am interested in data science as I want to be a data scientist in the field of banking and finance. With the help of data science , the firms are able to connect with the customers by knowing their choices, which can lead to increase  in their sales and hence increase their profit margin.*
+
+# ### Subtract 16 from 24
+
+# In[1]:
+
+
+24-16
+
+
+# |Serial no.|Names|Age|Designation|Job Address|Locations|
+# |---|-----|----|----|---|---|
+# |1|Anil|30|HR|Tanager Square|~~Brampton~~|
+# |2|Pooja|25|Teacher|Violet street|~~Waterloo~~|
+# |3|Gurjinder|29|Driver|Dufferin Street|~~Toronto~~|
+# |4|Manpreet|26|Machinist|Conestoga Drive|~~Brampton~~|
+# 
+# https://github.com/kaursimarjot96/testrepo/tree/main
+
+# In[ ]:
+
+
+
+
